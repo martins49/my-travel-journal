7afc4c5084a75c31f9c06239e7a84d1711c77a39
martins49 <martobusayo49@gmail.com>
2023-02-11 16:26:28 +0000

    second commit

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -5,7 +5,7 @@ const myDataArray = [
         country: "Japan",
         location: "Mount Fuji",
         date: "12 Jan, 2021 - 24 Jan, 2021",
-        description: "Mount Fug is the tallest mountain in Japan, standing at 3,276 meters (12,380 feet). Mount Fujis the single most popular tourist. site in Japan, for both Japanese and foreign tourists",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,276 meters (12,380 feet). Mount Fujis the single most popular tourist. site in Japan, for both Japanese and foreign tourists",
         link: "https://www.google.com/maps/place/Mount+Fuji/@35.3606237,138.7098538,14z/data=!4m14!1m7!3m6!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!2sMount+Fuji!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA"
         // mapIcon: "",
     },
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import React from "react"
 const Header = ()=>{
     return(
         <div className="header">
-            <img src="/Images/globe.jpg" alt=""  className="globe-img"/>  <span className="header-text">my travel journal</span>
+            <img src="/Images/globe.jpg" alt=""  className="globe-img"/>  <div className="header-text">my travel journal</div>
         </div>
 
     )
